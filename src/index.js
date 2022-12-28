ec8c675768db5bd4ff345e3a0bcd2304735da624
Arsh <mr.arsh026@gmail.com>
2022-12-28 21:53:40 +0530

    assignment

--- src/index.js
+++ src/index.js
@@ -7,19 +7,34 @@ mongoose.set('strictQuery', false)
 const app = express()
 
 const publicPath = path.join(__dirname, '../public')
+const viewPath = path.join(__dirname, '../views')
+
+
 app.use(express.static(publicPath))
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 
 mongoose.connect('mongodb+srv://arsh26:WZdIsZMdgBi1eOxE@cluster0.tytwkvw.mongodb.net/arshad',
 {useNewUrlParser: true})
 
 .then(() => console.log('MongoDB is connected'))
-.catch(err => console.log(err))
+.catch(error => console.log(error))
 
 
 app.use('/', route)
+
+
+app.get('/signup', (req, res) => {
+    res.render('signup')
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
 
  
 app.listen( 3000, function(){
